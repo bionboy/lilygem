092083dee6e277c5d4b22678f546a0c63ec2a057
Luke Floden <luke.floden@gmail.com>
2025-07-23 11:18:47 -0400

    docs: reminder of how to backfill data if i need to for exchange rate

---
+++ scripts/backfill-exchange-rates-db.ts
@@ -0,0 +1,5 @@
+// just:
+// 1. go to https://www.ofx.com/en-ca/forex-news/historical-exchange-rates/usd/cad/
+// 2. copy the table
+// 3. ask ai to make it into a csv
+// 4. import it into supabase
